7bfda3e0ea5ebb2b633dbae06516de775b5040af
thainguyen2k <nguyendragon2000@gmail.com>
2023-05-29 10:42:43 +0700

    update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,60 @@
-const Login=()=> {
-    return (
-      <div>
-        Login page
-      </div>
-    );
+import React from 'react'
+import styled from 'styled-components'
+import { FORM_ERROR } from 'final-form'
+import imgLogin from "img/imgLogin.png"
+import Logo from "component/logo"
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+  await sleep(300)
+  if (values.username !== 'Nguyen') {
+    return { username: 'Unknown username' }
   }
-  
-  export default Login;
+  if (values.password !== 'Nguyen') {
+    return { [FORM_ERROR]: 'Login Failed' }
+  }
+  window.alert('LOGIN SUCCESS!')
+}
+
+
+const Login = () => {
+  return(
+    <Body>
+      <img className='imgLogin' src={imgLogin}></img>
+      <form className='formLogin'>
+        <Logo></Logo>
+        <span className='login'>Log in</span>
+      </form>
+    </Body>
+  )
+}
+export default Login;
+
+const Body = styled.div`
+  background: linear-gradient(90deg, #FFFFFF 0%, #BBAAFF 66.67%);
+  width: 100%;
+  height: 753px;
+  position: relative;
+  background-repeat: no-repeat;
+  text-align: center;
+  .imgLogin{
+    position: absolute;
+    width: auto;
+    height: 753px;
+    left: 50%;
+  }
+  .formLogin{
+    width: 50%;
+    height: 750px;
+    border: 2px solid red;
+  }
+  .login{
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 56px;
+    line-height: 84px;
+    color: #000000;
+  }
+`
---
+++ src/component/logo/logo.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import logo from "img/Logo.png"
+
+const Logo = () => {
+    return (
+        <StyleL>
+            <img src={logo} alt="logo"></img>
+            <div>
+                <div className="logo-text">MyNFT</div>
+                <div className="logo-desc">NFT Marketplace</div>
+            </div>
+        </StyleL>
+    )
+}
+
+export default Logo;
+
+const StyleL = styled.div`
+  flex-direction: row;
+  margin-top: 80px;
+  .logo {
+    align-items: center;
+    gap: 16px;
+  }
+  .logo-text {
+    font-weight: 700;
+    font-size: 28px;
+    line-height: 36px;
+    color: #27262E;
+  }
+  .logo-desc {
+    font-size: 10px;
+    line-height: 13px;
+    color: #7A797D;
+  }
+`;
